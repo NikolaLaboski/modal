41d214f87c8f092e584d08b95e4c76692c48fe15
Nikola Laboski <nikola.laboski@outlook.com>
2025-04-04 22:23:19 +0200

    updated html paragraph

--- app.js
+++ app.js
@@ -4,27 +4,13 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
+const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click", () => {
-  modal.classList.add("open-modal");
+  modalOverlay.classList.add("open-modal");
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.classList.remove("open-modal");
+  modalOverlay.classList.remove("open-modal");
 });
-
-/*
-const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
-
-modalBtn.addEventListener("click", function(){
-    modal.classList.add(".open-modal");
-});
-
-closeBtn.addEventListener("click",function(){
-    modal.classList.remove("open-modal");
-});
-*/
